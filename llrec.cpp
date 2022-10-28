600fc5ff85cbfb2b9db7a95b063acee197d8b571
Karla Gonzalez <karlalg@usc.edu>
2022-10-28 02:38:31 +0000

    All done with checkpoint

--- llrec.cpp
+++ llrec.cpp
@@ -3,5 +3,22 @@
 //*********************************************
 // Provide your implementation of llpivot below
 //*********************************************
-
+void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot){
+  if(head==nullptr)
+  {
+    smaller= nullptr;
+    larger= nullptr; 
+    return; 
+  }
+  if(head->val <= pivot)
+  {
+    smaller= head; 
+    llpivot(head->next, smaller->next, larger, pivot);
+  }
+  if(head->val > pivot)
+  {
+    larger= head;
+    llpivot(head->next, smaller, larger->next, pivot);
+  }
+}
 
